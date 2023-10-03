3b019b08e0b76726a885d88e531721e8e317cdc5
Rajesh G <rajhseg@gmail.com>
2023-10-03 09:28:07 +0530

    binary search
    Binary Search

---
+++ BinarySearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class BinarySearch
+    {
+        public static int GetIndex(int[] sortedArray, int searchValue)
+        {
+            return Get(sortedArray, searchValue, sortedArray.Length / 2, sortedArray.Length-1);
+        }
+
+        public static int Get(int[] arr, int value, int middleIndex, int binaryLastIndex)
+        {            
+            if(middleIndex == binaryLastIndex)
+            {             
+                if (arr[binaryLastIndex] == value)
+                    return binaryLastIndex;
+                else
+                    return -1;
+            }          
+            else if(arr[middleIndex] < value)
+            {
+                return Get(arr, value,(int) Math.Ceiling((middleIndex + binaryLastIndex)/(double)2), binaryLastIndex);
+            }
+            else if(arr[middleIndex]> value){
+                return Get(arr, value, middleIndex / 2, middleIndex);
+            }
+            else
+            {
+                if (arr[middleIndex] == value)
+                {
+                    return middleIndex;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+}
