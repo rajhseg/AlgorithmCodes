5fd3243e710e4a055444331d908917e84a2343a9
Rajesh G <rajhseg@gmail.com>
2023-10-05 08:27:04 +0530

    binary tree
    binary tree

---
+++ CustomBinaryNode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomBinaryNode<T> where T: IComparable<T>, new()
+    {
+        private CustomBinaryNode<T> left;
+        private CustomBinaryNode<T> right;
+
+        public T Value { get; set; }
+
+        public CustomBinaryNode<T> Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+
+        public CustomBinaryNode<T> Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        public CustomBinaryNode(T value)
+        {
+            this.Value = value;
+            this.Left = null;
+            this.Right = null;
+        }
+    }
+}
---
+++ CustomBinaryTree.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomBinaryTree<T> where T:IComparable<T>,new()
+    {
+        public CustomBinaryNode<T> Root { get; set; }
+
+        public CustomBinaryTree()
+        {
+
+        }
+
+        public CustomBinaryNode<T> InsertValue(T value)
+        {            
+            return TraversalInsert(value);
+        }
+
+        private CustomBinaryNode<T> TraversalInsert(T value)
+        {
+            if (Root == null)
+            {
+                Root = new CustomBinaryNode<T>(value);                
+                return Root;
+            }
+
+            Root = InsertRecursive(Root, value);
+
+            return Root;
+        }
+
+        private CustomBinaryNode<T> InsertRecursive(CustomBinaryNode<T> current, T data)
+        {
+            if (current == null)
+            {
+                return new CustomBinaryNode<T>(data);
+            }
+
+            if (data.CompareTo(current.Value) == -1)
+            {
+                current.Left = InsertRecursive(current.Left, data);
+            }
+            else if (data.CompareTo(current.Value)==1)
+            {
+                current.Right = InsertRecursive(current.Right, data);
+            }
+
+            return current;
+        }
+
+        public CustomBinaryNode<T> SearchNode(T value)
+        {
+            return TraverseNode(Root, value);
+        }
+
+        public bool ContainsValue(T value)
+        {
+            var node = SearchNode(value);
+            return node != null;
+        }
+
+        public void PrintInOrderTree()
+        {
+            Traversal(Root, BinaryTreeTraversal.InOrder);
+        }
+
+        public void PrintPreOrderTree()
+        {
+            Traversal(Root, BinaryTreeTraversal.PreOrder);
+        }
+
+        public void PrintPostOrderTree()
+        {
+            Traversal(Root, BinaryTreeTraversal.PostOrder);
+        }
+
+        private void Traversal(CustomBinaryNode<T> node, BinaryTreeTraversal traversal)
+        {
+            if (node == null)
+                return;
+
+            if (traversal == BinaryTreeTraversal.InOrder)
+            {
+                Traversal(node.Left, traversal);
+                Console.WriteLine("--" + node.Value + "--");
+                Traversal(node.Right, traversal);
+            }
+            else if(traversal == BinaryTreeTraversal.PreOrder)
+            {
+                Console.WriteLine("--" + node.Value + "--");
+                Traversal(node.Left, traversal);               
+                Traversal(node.Right, traversal);
+            }
+            else
+            {
+                Traversal(node.Left, traversal);                
+                Traversal(node.Right, traversal);
+                Console.WriteLine("--" + node.Value + "--");
+            }
+        }
+
+        public void TraverseInOrder(CustomBinaryNode<T> node, List<T> list)
+        {
+            if (node == null)
+                return;
+
+            TraverseInOrder(node.Left, list);
+            list.Add(node.Value);
+            TraverseInOrder(node.Right, list);
+        }
+
+        private CustomBinaryNode<T> TraverseNode(CustomBinaryNode<T> node, T value)
+        {
+            if (node.Value.CompareTo(value) == 0)
+            {
+                return node;
+            }
+            else if (node.Value.CompareTo(value) == 1)
+            {
+                return TraverseNode(node.Right, value);
+            }
+            else if (node.Value.CompareTo(value) == -1)
+            {
+                return TraverseNode(node.Left, value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    public enum BinaryTreeTraversal
+    {
+        InOrder,
+        PreOrder,
+        PostOrder
+    }
+}
