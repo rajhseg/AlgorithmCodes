bf1db23be7c4aaaa4eedc6087dd94c6aa119d0de
Rajesh G <rajhseg@gmail.com>
2023-10-20 08:58:05 +0530

    LinkedList
    LinkedList

---
+++ CustomLinkedList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class CustomLinkedList<T> : IEnumerable<T>, IDisposable where T:IComparable<T>, new()
+    {
+        private CustomLinkedListNode<T> headNode;
+        private CustomLinkedListNode<T> currentLastNode;
+
+        public CustomLinkedListNode<T> Root
+        {
+            get { return headNode; }
+        }
+
+        public CustomLinkedList()
+        {
+
+        }
+
+        public void AddNode(T value)
+        {
+            var node = new CustomLinkedListNode<T>(value);
+
+            if (headNode == null)
+            {
+                headNode = node;
+                currentLastNode = headNode;
+            }
+            else
+            {                
+                currentLastNode.NextNode = node;
+                node.PreviousNode = currentLastNode;
+                node.NextNode = default;
+                
+                currentLastNode = node;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new CustomLinkedListEnumerator<T>(headNode);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Dispose()
+        {
+            headNode = null;
+            currentLastNode = null;
+        }
+    }
+
+    public class CustomLinkedListEnumerator<T> : IEnumerator<T> where T:IComparable<T>, new()
+    {
+        private CustomLinkedListNode<T> _root;
+        private CustomLinkedListNode<T> _currentNode;
+
+        public CustomLinkedListEnumerator(CustomLinkedListNode<T> root)
+        {
+            _root = root;            
+        }
+
+        public T Current => _currentNode.Value;
+
+        object IEnumerator.Current => _currentNode.Value;
+
+        public void Dispose()
+        {
+            _root = null;
+            _currentNode = null;
+        }
+
+        public bool MoveNext()
+        {
+            CustomLinkedListNode<T> nextNode;
+
+            if (_currentNode == null)
+            {
+                 nextNode = _root;
+                _currentNode = nextNode;
+                return nextNode!=null;
+            }
+            else
+            {
+                _currentNode = _currentNode.NextNode;
+            }
+                                
+            return _currentNode!=null;
+        }
+
+        public void Reset()
+        {
+            _root = null;
+        }
+    }
+}
---
+++ CustomLinkedListNode.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class CustomLinkedListNode<T> : IComparable<T> where T: IComparable<T>, new()
+    {
+        private T _value;
+
+        public T Value
+        {
+            get { return _value; }
+            set { _value = value; }
+        }
+
+        private CustomLinkedListNode<T> _prevNode;
+
+        public CustomLinkedListNode<T> PreviousNode
+        {
+            get { return _prevNode; }
+            set { _prevNode = value; }
+        }
+
+        private CustomLinkedListNode<T> _nextNode;
+
+        public CustomLinkedListNode<T> NextNode
+        {
+            get { return _nextNode; }
+            set
+            {
+                _nextNode = value;
+            }
+        }
+
+        public CustomLinkedListNode(T value)
+        {            
+            Value = value;
+        }
+
+        public int CompareTo(T other)
+        {
+            return Value.CompareTo(other);
+        }
+    }
+}
